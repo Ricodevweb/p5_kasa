22834401dbbc27d594de67ca6cce6b7d33836828
ericbdev2024 <ericbourobou@gmail.com>
2024-12-23 14:37:53 +0100

    modif composants

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,31 @@
+// Importation de React, nécessaire pour créer un composant React
 import React from 'react';
+
+// Importation du fichier CSS pour appliquer le style
 import './Card.css';
+
+// Importation de 'Link' depuis 'react-router-dom' pour créer des liens de navigation
 import { Link } from 'react-router-dom';
 
-// Card component declaration with a function
+// Déclaration du composant Card sous forme de fonction
 function Card(props) {
-   	return (
-		<article className='cards-in-home-page'>
-			<Link to={`/logement/${props.id}`}>
-				<div className='overlay'></div>
-				<img src={props.cover} alt={props.title} title={props.title} />
-				<h3>{props.title}</h3>
-			</Link>
-		</article>
-	);
+    return (
+        // Article qui représente une carte sur la page d'accueil
+        <article className='cards-in-home-page'>
+            {/* Lien pour la navigation, redirige vers la page de détail du logement */}
+            <Link to={`/logement/${props.id}`}>
+                {/* Div utilisée pour l'overlay (superposition transparente) sur l'image */}
+                <div className='overlay'></div>
+
+                {/* Image du logement avec source dynamique via 'props.cover' */}
+                <img src={props.cover} alt={props.title} title={props.title} />
+
+                {/* Titre du logement affiché dans chaque carte */}
+                <h3>{props.title}</h3>
+            </Link>
+        </article>
+    );
 };
 
+// Exportation du composant Card pour qu'il puisse être utilisé ailleurs dans l'application
 export default Card;
--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -2,28 +2,30 @@ import React, { useState } from 'react';
 import './Collapse.css';
 import arrowopen from '../../assets/arrowopen.png';
 
-// Collapse component declaration with a function 
+// Déclaration du composant Collapse sous forme de fonction
 function Collapse(props) {
-	
-	// Definition of the local state of `is open` using the useState hook
-	const [isOpen, setIsOpen] = useState(props.open !== undefined ? props.open: false);
-	// Creation of an event management function that will update the value of isOpen when the user clicks on one of the collapses
-	const handleClick = () => {
-		setIsOpen(!isOpen);
-	};
-   
-	return (
-		<div className='collapse-drop-down-list'>
-			<h3 onClick={handleClick}>
-				{props.title}
-				<img className={isOpen ? 'arrow arrow-to-close': 'arrow arrow-to-open'}
-					src={arrowopen}
-					alt="show content"
-				/>
-			</h3>
-			{isOpen && <div className='content-in-collapse'>{props.content}</div>}
-	  	</div>
-  	);
+    // Définition de l'état local `isOpen` en utilisant le hook useState
+    const [isOpen, setIsOpen] = useState(props.open !== undefined ? props.open : false);
+
+    // Création d'une fonction de gestion d'événements qui met à jour la valeur de isOpen lorsque l'utilisateur clique sur un des collapses
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={`collapse-drop-down-list ${isOpen ? 'open' : ''}`}>
+            <h3 onClick={handleClick}>
+                {props.title}
+                <img className={isOpen ? 'arrow arrow-to-close' : 'arrow arrow-to-open'}
+                     src={arrowopen}
+                     alt="afficher le contenu"
+                />
+            </h3>
+            <div className={`content-in-collapse ${isOpen ? 'open' : ''}`}>
+                {props.content}
+            </div>
+        </div>
+    );
 };
 
 export default Collapse;
