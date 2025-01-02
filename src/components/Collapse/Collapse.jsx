13e26283688d3f6a4099c218c0528af4bfd540ce
ericbdev2024 <ericbourobou@gmail.com>
2025-01-02 22:35:34 +0100

    modif components collapse.css, collapse.jsx, footesr.css

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-import './Collapse.css';
-import arrowopen from '../../assets/arrowopen.png';
-
-// Déclaration du composant Collapse sous forme de fonction
+import React, { useState, useRef } from "react";
+import "./Collapse.css";
+import arrowopen from "../../assets/arrowopen.png";
 function Collapse(props) {
-    // Définition de l'état local `isOpen` en utilisant le hook useState
-    const [isOpen, setIsOpen] = useState(props.open !== undefined ? props.open : false);
-
-    // Création d'une fonction de gestion d'événements qui met à jour la valeur de isOpen lorsque l'utilisateur clique sur un des collapses
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
     const handleClick = () => {
         setIsOpen(!isOpen);
     };
-
     return (
-        <div className={`collapse-drop-down-list ${isOpen ? 'open' : ''}`}>
+        <div className={`collapse-drop-down-list ${isOpen ? "open" : ""}`}>
             <h3 onClick={handleClick}>
                 {props.title}
-                <img className={isOpen ? 'arrow arrow-to-close' : 'arrow arrow-to-open'}
-                     src={arrowopen}
-                     alt="afficher le contenu"
+                <img
+                    className={
+                        isOpen ? "arrow arrow-to-close" : "arrow arrow-to-open"
+                    }
+                    src={arrowopen}
+                    alt="afficher le contenu"
                 />
             </h3>
-            <div className={`content-in-collapse ${isOpen ? 'open' : ''}`}>
+            <div
+                className="content-in-collapse"
+                ref={contentRef}
+                style={{
+                    maxHeight: isOpen
+                        ? `${contentRef.current.scrollHeight}px`
+                        : "0",
+                }}
+            >
                 {props.content}
             </div>
         </div>
     );
-};
-
+}
 export default Collapse;
